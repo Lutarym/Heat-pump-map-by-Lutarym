09ce77e5ef317a301ac2c8144899737ba48b8344
Stephan <stephan.froebe@pm.me>
2026-08-25 23:04:56 +0200

    Update lutarym-heatpump-card.js

--- dist/lutarym-heatpump-card.js
+++ dist/lutarym-heatpump-card.js
@@ -1705,8 +1705,8 @@ class LutarymHeatpumpCard extends HTMLElement {
             <circle r="4" fill="#0D1219"/>
           </g>
         </g>
-        <text class="cap-s" x="1345" y="340" text-anchor="end">Zirkulation</text>
-        <text class="value-s" id="zirk-v" x="1345" y="366" text-anchor="end">--</text>
+        <text class="cap-s" x="1370" y="320" text-anchor="middle">Zirkulation</text>
+        <text class="value-s" id="zirk-v" x="1335" y="366" text-anchor="end">--</text>
       </g>
 
 
