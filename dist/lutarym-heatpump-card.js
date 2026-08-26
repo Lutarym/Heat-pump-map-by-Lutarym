9a65d71f53cb30d273fc7d1c8591766cbe86c257
Stephan <stephan.froebe@pm.me>
2026-08-26 12:16:24 +0200

    fix

--- dist/lutarym-heatpump-card.js
+++ dist/lutarym-heatpump-card.js
@@ -1678,9 +1678,14 @@ class LutarymHeatpumpCard extends HTMLElement {
       <!-- Warmwasserspeicher -->
       <!-- Zirkulationskreis am Warmwasserspeicher -->
       <g id="zirkulation-group" opacity="0">
-        <path class="pipe-shell" fill="none" d="M1440 320 H 1370 M1370 320 V 560 M1370 560 H 1440"/>
-        <path class="pipe" id="pipe-zirk" fill="none" d="M1440 320 H 1370 M1370 320 V 560 M1370 560 H 1440"/>
-        <path class="flowdots" id="dots-zirk" fill="none" d="M1440 320 H 1370 M1370 320 V 560 M1370 560 H 1440"/>
+        <path class="pipe-shell" fill="none" d="M1440 320 H 1370 M1370 560 H 1440"/>
+        <path class="pipe-shell" fill="none" d="M1370 320 V 560"/>
+        <path class="pipe" id="pipe-zirk-h1" fill="none" d="M1440 320 H 1370"/>
+        <path class="pipe" id="pipe-zirk-v" fill="none" d="M1370 320 V 560"/>
+        <path class="pipe" id="pipe-zirk-h2" fill="none" d="M1370 560 H 1440"/>
+        <path class="flowdots" id="dots-zirk-h1" fill="none" d="M1440 320 H 1370"/>
+        <path class="flowdots" id="dots-zirk-v" fill="none" d="M1370 320 V 560"/>
+        <path class="flowdots" id="dots-zirk-h2" fill="none" d="M1370 560 H 1440"/>
         <g transform="translate(1370 360)">
           <circle r="24" fill="#0D1219" stroke="#33415A" stroke-width="2"/>
           <g class="rotor" id="zirk-rotor">
